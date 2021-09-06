148409c58afc8c39efe4898ae37f2d4abbef83e5
Alexey <AAnaKireev@sberbank.ru>
2021-09-06 17:53:23 +0300

    - Произведена доработка в соответствии с замечаниями

--- qa/src/test/kotlin/ru/sber/qa/CertificateRequestTest.kt
+++ qa/src/test/kotlin/ru/sber/qa/CertificateRequestTest.kt
@@ -3,25 +3,37 @@ package ru.sber.qa
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkObject
+import io.mockk.unmockkAll
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import kotlin.random.Random
 
 internal class CertificateRequestTest {
 
+
+    private var hrEmployeeNumber = 20L
     private var employeeNumber = 8L
     private var certificateType = mockk<CertificateType>()
     private var certificateRequest = CertificateRequest(employeeNumber, certificateType)
 
+    @BeforeEach
+    fun beforeTests() {
+        mockkObject(Scanner)
+    }
+
     @Test
     fun process() {
-        var hrEmployeeNumber = 8L
         var byteArr = Random.nextBytes(100)
-        mockkObject(Scanner)
 
         every { Scanner.getScanData() } returns byteArr
         assertNotNull(certificateRequest.process(hrEmployeeNumber))
+    }
+
+    @Test
+    fun throwScanTimeoutException() {
         every { Scanner.getScanData() } throws ScanTimeoutException()
         assertThrows(ScanTimeoutException::class.java, { certificateRequest.process(hrEmployeeNumber) })
     }
@@ -34,5 +46,10 @@ internal class CertificateRequestTest {
     @Test
     fun getCertificateType() {
         assertEquals(certificateType, certificateRequest.certificateType)
+    }
+
+    @AfterEach
+    fun afterTests() {
+        unmockkAll()
     }
 }
--- qa/src/test/kotlin/ru/sber/qa/ScannerTest.kt
+++ qa/src/test/kotlin/ru/sber/qa/ScannerTest.kt
@@ -1,17 +1,24 @@
 package ru.sber.qa
 
 import io.mockk.*
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import kotlin.random.Random
 import kotlin.test.assertEquals
 
 internal class ScannerTest {
+
+    @BeforeEach
+    fun beforeTests() {
+        mockkObject(Random)
+    }
+
     @Test
     fun getScanData() {
         var expected = Random.nextBytes(100)
-        mockkObject(Random)
         every { Random.nextLong(5000L, 15000L) } returns 1L
 
         assertDoesNotThrow({ Scanner.getScanData() })
@@ -20,9 +27,13 @@ internal class ScannerTest {
 
     @Test
     fun throwScanTimeoutException() {
-        mockkObject(Random)
         every { Random.nextLong(5000L, 15000L) } returns Scanner.SCAN_TIMEOUT_THRESHOLD + 1
 
         assertThrows(ScanTimeoutException::class.java, { Scanner.getScanData() })
+    }
+
+    @AfterEach
+    fun afterTests() {
+        unmockkAll()
     }
 }
