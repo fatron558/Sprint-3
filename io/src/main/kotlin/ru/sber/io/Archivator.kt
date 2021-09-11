128e07ded4e81ed025cf678d828afccc26b1592d
Alexey <AAnaKireev@sberbank.ru>
2021-09-11 11:47:36 +0300

    готово

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,5 +1,11 @@
 package ru.sber.io
 
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
@@ -10,15 +16,40 @@ class Archivator {
      * Метод, который архивирует файл logfile.log в архив logfile.zip.
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
-    fun zipLogfile() {
+    fun zipLogfile(filePath: String = "io/logfile.log", zipFile: String = "io/logfile.zip") {
+        val zout = ZipOutputStream(FileOutputStream(zipFile))
+        val fis = FileInputStream(filePath)
+        val buffer: ByteArray
+        val zipEntry = ZipEntry(filePath)
 
+        fis.use {
+            buffer = fis.readBytes()
+        }
+
+        zout.use {
+            zout.putNextEntry(zipEntry)
+            zout.write(buffer)
+            zout.closeEntry()
+        }
     }
 
     /**
      * Метод, который извлекает файл из архива.
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
-    fun unzipLogfile() {
+    fun unzipLogfile(unzippedFile: String = "io/logfileUnzip.log", zipFile: String = "io/logfile.zip") {
+        val zis = ZipInputStream(FileInputStream(zipFile))
+        val fout = FileOutputStream(unzippedFile)
+        val buffer: ByteArray
 
+        zis.use {
+            zis.nextEntry
+            buffer = zis.readBytes()
+            zis.closeEntry()
+        }
+
+        fout.use {
+            fout.write(buffer)
+        }
     }
 }
